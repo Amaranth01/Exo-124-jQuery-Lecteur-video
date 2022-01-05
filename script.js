59b6104604d7b3d925ef1a8851ff3993ada5711c
Amaranth01 <vanessa.nectoux@gmail.com>
2022-01-05 10:53:38 +0100

    step 2

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
 let player = $('.player__video').first();
 let progress = $('.progress__filled').first();
+let toggle = true;
+
+//button play pause
+$(".toggle").click(function (){
+    if (toggle){
+        $(this).html("⏸");
+        toggle = false;
+        player.get(0).play();
+    }
+    else {
+        $(this).html("►");
+        toggle = true;
+        player.get(0).pause();
+    }
+});
+
+player.on('timeupdate', function (){
+    let maxBar = 200;
+    let currentBar = 0;
+    let progressBar;
+    let intervalId;
+
+    let initialisation = function() {
+        progressBar = document.getElementById( "progressBar" );
+        progressBar.value = currentBar;
+        progressBar.max = maxBar;
+    }
+
+    let displayBar = function() {
+        currentBar++;
+        progressBar.value = currentBar;
+    }
+
+    intervalId = setInterval( displayBar , 100 );
+});
+
+//progress filled
+$("#playbackRate").on("change", function (){
+    player[0].currentTime = document.getElementById("playbackRate").value * player[0].duration;
+});
+
+//back -10 or speed 25
+$("button").click(function (){
+    switch ($(this).attr("data-skip")){
+        case "-10":
+            player[0].currentTime -= 10;
+            break;
+        case "25":
+            player[0].currentTime += 25;
+            break;
+    }
+});
+
+//sound filled
+$("#volume").on("change", function (){
+    player[0].volume = this.value;
+});
